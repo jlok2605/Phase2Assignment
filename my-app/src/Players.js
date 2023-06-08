35f8f9f714873fa0fc09831c7e01463bdf168ed8
jlok2605 <124659984+jlok2605@users.noreply.github.com>
2023-06-08 22:00:44 +1000

    Added files with working routes and players

---
+++ my-app/src/Players.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Players (){
+    return (
+    <div>        
+        <div id = "1" >
+            <img src ={"/images/VA.jpg"}  alt = "Viktor Axelsen"></img>
+        </div>
+
+         <div id = "2" alt =" AG">
+            <img src = {"/images/2AG.jpg"} alt = "Anthony Ginting"></img>
+        </div>
+
+        <div id = "3" alt = "KN">
+            <img src = {"/images/3KN.jpg"} alt =  'Kodai Naraoka'></img>
+        </div>
+
+        <div id = "4" alt = "LKY">
+            <img src = {"/images/4LKY.jpg"} alt = "Kean Yew Loh"></img>
+        </div>
+
+        <div id = "5" alt = "KV">
+            <img src = {"/images/5KV.jpg"} alt = "Kunlavut Vitidsarn" ></img>
+        </div>
+
+        <div id = "6" alt = "JC">
+            <img src = {"/images/6JC.jpg"} alt = "Jonatan Christie"></img>
+        </div>
+
+        <div id = "7" alt = "TCC">
+            <img src = {"/images/7TCC.jpg"} alt = "Tien Chen Chou"></img>
+        </div>
+
+        <div id = "8" alt = "HSP">
+            <img src = {"/images/8HSP.jpg"} alt = "H.S. Prannoy"></img>
+        </div>
+
+        <div id = "9" alt = "SYQ">
+            <img src = {"/images/9SYQ.jpg"} alt = "Yu Qi Shi"></img>
+        </div>
+
+        <div id = "10" alt = 'LZJ'>
+            <img src = {"/images/LZJ.jpg"} alt = "Zii Jia Lee"></img>
+        </div>
+
+</div>
+    )
+}
+
+export default Players;
---
+++ my-app/src/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Home(){
+return (
+    <div>
+    <h1>Home</h1>
+    <p>Welcome to the badminton home page for the World ranking for the current men's single standing.</p>
+    </div>
+)
+    
+}
+
+export default Home;
