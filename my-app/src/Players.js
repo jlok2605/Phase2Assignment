144028a0ec054007670cfa06096f574d0ee6b1c4
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-06-11 10:45:15 +1000

    players inline

--- my-app/src/Players.js
+++ my-app/src/Players.js
@@ -6,47 +6,48 @@ import "./Players.css"
 function Players (){
     return (
     <div> 
-        <p>Testing</p>
-        <div id = "VA" class = "pic" >
-            <img src ={"/images/VA.jpg"}  alt = "Viktor Axelsen"></img>
-        </div>
+        <span class = 'top5'>
+            <div id = "VA" class = "pic" >
+                <img src ={"/images/VA.jpg"}  alt = "Viktor Axelsen"></img>
+            </div>
 
-         <div id = "2" alt =" AG"  class = "pic">
-            <img src = {"/images/2AG.jpg"} alt = "Anthony Ginting"></img>
-        </div>
+            <div id = "2" alt =" AG"  class = "pic">
+                <img src = {"/images/2AG.jpg"} alt = "Anthony Ginting"></img>
+            </div>
 
-        <div id = "3" alt = "KN"  class = "pic">
-            <img src = {"/images/3KN.jpg"} alt =  'Kodai Naraoka'></img>
-        </div>
+            <div id = "3" alt = "KN"  class = "pic">
+                <img src = {"/images/3KN.jpg"} alt =  'Kodai Naraoka'></img>
+            </div>
 
-        <div id = "4" alt = "LKY"  class = "pic">
-            <img src = {"/images/4LKY.jpg"} alt = "Kean Yew Loh" ></img>
-        </div>
+            <div id = "4" alt = "LKY"  class = "pic">
+                <img src = {"/images/4LKY.jpg"} alt = "Kean Yew Loh" ></img>
+            </div>
 
-        <div id = "5" alt = "KV"  class = "pic">
-            <img src = {"/images/5KV.jpg"} alt = "Kunlavut Vitidsarn" ></img>
-        </div>
+            <div id = "5" alt = "KV"  class = "pic">
+                <img src = {"/images/5KV.jpg"} alt = "Kunlavut Vitidsarn" ></img>
+            </div>
+        </span>
+        <span class = 'lower5'>
+            <div id = "6" alt = "JC" class = "pic">
+                <img src = {"/images/6JC.jpg"} alt = "Jonatan Christie"></img>
+            </div>
 
-        <div id = "6" alt = "JC" class = "pic">
-            <img src = {"/images/6JC.jpg"} alt = "Jonatan Christie"></img>
-        </div>
+            <div id = "7" alt = "TCC" class = "pic">
+                <img src = {"/images/7TCC.jpg"} alt = "Tien Chen Chou"></img>
+            </div>
 
-        <div id = "7" alt = "TCC" class = "pic">
-            <img src = {"/images/7TCC.jpg"} alt = "Tien Chen Chou"></img>
-        </div>
+            <div id = "8" alt = "HSP"  class = "pic">
+                <img src = {"/images/8HSP.jpg"} alt = "H.S. Prannoy"></img>
+            </div>
 
-        <div id = "8" alt = "HSP"  class = "pic">
-            <img src = {"/images/8HSP.jpg"} alt = "H.S. Prannoy"></img>
-        </div>
+            <div id = "9" alt = "SYQ" class = "pic">
+                <img src = {"/images/9SYQ.jpg"} alt = "Yu Qi Shi"></img>
+            </div>
 
-        <div id = "9" alt = "SYQ" class = "pic">
-            <img src = {"/images/9SYQ.jpg"} alt = "Yu Qi Shi"></img>
-        </div>
-
-        <div id = "10" alt = 'LZJ' class = "pic">
-            <img src = {"/images/LZJ.jpg"} alt = "Zii Jia Lee"></img>
-        </div>
-
+            <div id = "10" alt = 'LZJ' class = "pic">
+                <img src = {"/images/LZJ.jpg"} alt = "Zii Jia Lee"></img>
+            </div>
+        </span>
 
 </div>
     )
