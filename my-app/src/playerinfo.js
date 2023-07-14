a285a9a9d8636a73eebd2127ae40ce96dafac20b
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-07-14 22:33:14 +1000

    css working stats over players

--- my-app/src/playerinfo.js
+++ my-app/src/playerinfo.js
@@ -6,14 +6,16 @@ function PlayerInfo(props) {
 
   
   return (
-  <div>
-  <div className = "pic" >
+  <div className='container'>
+  <div >
       <img src ={player.picture}  alt = {player.name}></img>
+      <div className='stats'>
         <h3>Name: {player.name}</h3>
         <h3>Rank: {player.rank}</h3>
         <h3>Points: {player.points}</h3>
         <h3>Tournaments: {player.tournaments}</h3>
         <h3>Records:{player.record}</h3>
+      </div>
   </div>
   </div>
 
